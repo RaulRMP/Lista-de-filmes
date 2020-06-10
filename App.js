3a61384bf36cde30abea822954883fb7d0dbda1c
RaulPereira <raulgrota@hotmail.com.br>
2020-06-09 21:02:26 -0300

    novo

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, FlatList, ActivityIndicator } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import api from "./src/services/api";
+
+import Filmes from "./src/Filmes/index";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmes: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get("r-api/?api=filmes");
+    this.setState({
+      filmes: response.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.loading}>
+          <ActivityIndicator color="#09A6FF" size={40} />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={this.state.filmes}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <Filmes data={item} />}
+          />
+        </View>
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  loading: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
   },
 });
+
+export default App;
